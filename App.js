1ab93e46ef297c846373c866c4521fb418343b4a
ashwinthomas03 <118752427+ashwinthomas03@users.noreply.github.com>
2023-12-11 00:59:52 -0500

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Button, View } from "react-native";
+import { Button, View, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Login from "./Login";
 import SignUp from "./SignUp";
@@ -13,25 +13,30 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen
-        name="Login"
-        component={Login}
-        options={{
-          header: () => (
-            <View style={{ flexDirection: "column", margin: 20 }}>
-              {/* Button to navigate to SignUp page */}
-              <SignUpButton />
-            </View>
-          ),
-        }}
-      />
-      <Stack.Screen name="SignUp" component={SignUp} />
-      <Stack.Screen name="Home" component={Home} />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
-};
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerTitle: "Login", // Set the header title for the Login screen
+            headerRight: () => (
+              <View style={{ flexDirection: "row", marginRight: 10 }}>
+                {/* Button to navigate to SignUp page */}
+                <SignUpButton />
+              </View>
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ title: "Sign Up" }} // Set the header title for the SignUp screen
+        />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 const SignUpButton = () => {
   const navigation = useNavigation();
@@ -40,7 +45,7 @@ const SignUpButton = () => {
     navigation.navigate("SignUp");
   };
 
-  return <Button title="Go to Sign Up" onPress={handleSignUpPress} />;
+  return <Button title="Sign Up" onPress={handleSignUpPress} />;
 };
 
 export default App;
