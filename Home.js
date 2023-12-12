a5035d9facabd70dbf9549982960e54bb82b115a
ashwinthomas03 <118752427+ashwinthomas03@users.noreply.github.com>
2023-12-11 23:52:58 -0500

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,24 +1,88 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import React, { useState, useEffect } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import axios from "axios";
 
-const Home = () => {
+function Home() {
   const navigation = useNavigation();
+  const route = useRoute();
+  const [memberInfo, setMemberInfo] = useState(null);
+  const { username } = route.params;
 
-  const handleSignUpPress = () => {
-    //fix
+  useEffect(() => {
+    axios.get(`http://localhost:3000/member/${username}`)
+      .then(response => {
+        setMemberInfo(response.data.memberInfo);
+      })
+      .catch(error => {
+        console.error("Error fetching member info:", error);
+      });
+  }, [username]);
+
+  const goToBMIpage = () => {
+    navigation.navigate("BMIpage");
   };
 
   return (
-    <View>
-      <Text>Home Page</Text>
-
-      {/* Add any content or features specific to your home page here */}
-
-      {/* Example button to navigate to SignUp page */}
-      
+    <View style={styles.container}>
+      {memberInfo && (
+        <View style={styles.profileCard}>
+          <Text style={styles.welcomeText}>Welcome, {memberInfo.Full_name}</Text>
+          <Text style={styles.cardText}>Full Name: {memberInfo.Full_name}</Text>
+          <Text style={styles.cardText}>Age: {memberInfo.age}</Text>
+          <Text style={styles.cardText}>Gender: {memberInfo.gender}</Text>
+          <Text style={styles.cardText}>DOB: {memberInfo.DOB}</Text>
+          <Text style={styles.cardText}>Email: {memberInfo.email}</Text>
+          <Text style={styles.cardText}>Phone Number: {memberInfo.phone_number}</Text>
+        </View>
+      )}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={goToBMIpage}
+      >
+        <Text style={styles.buttonText}>Go to BMI Calculator</Text>
+      </TouchableOpacity>
     </View>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#eef2f3",
+    padding: 20,
+    position: "relative",
+  },
+  welcomeText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  profileCard: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 20,
+    elevation: 5,
+    position: "absolute",
+    top: 20,
+    left: 20,
+  },
+  cardText: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "#2596be",
+    padding: 15,
+    borderRadius: 5,
+    alignSelf: "center",
+    position: "absolute",
+    bottom: 20,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
 
 export default Home;
