4066f8574f44a9eb17c582fbf5be8d981a2fb6ef
tabdin03 <150469000+tabdin03@users.noreply.github.com>
2023-12-12 19:43:16 -0500

    Add files via upload

--- WorkoutPage.js
+++ WorkoutPage.js
@@ -1,6 +1,6 @@
+
 import React, { useState } from 'react';
-import { View, Text, Picker, Image, Button } from 'react-native';
-import WorkoutPage from './SignUp';
+import { View, Text, Picker, Image, Button, TouchableOpacity, StyleSheet } from 'react-native';
 
 const WorkoutPage = () => {
   const [muscleFocus, setMuscleFocus] = useState('');
@@ -17,94 +17,157 @@ const WorkoutPage = () => {
   };
 
   return (
-    <View style={{ flex: 1, padding: 20, backgroundColor: "#F2856D" }}>
-       
-       <View style={{ flexDirection: 'coloumn', justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ 
-        textAlign: "center" ,
-        fontSize: 24,
-        fontWeight: 'bold', 
-        textShadowColor:'#F2C49B', 
-        textShadowOffset:{width: 1, height: 1}, 
-        textShadowRadius: 2, 
-        // fontFamily: 'SpaceGrotesk'
-        }}>SmartFit</Text>
-        
-        <Image
-          source={require('./assets/logo.png')}
-          style={{ width: 200, height: 200, borderRadius: 15 }}
-        />
-      </View>
-      <Text style={{paddingBottom: 3, 
-        paddingLeft: 15, 
-        color: '#F2C49B', 
-        fontSize: 18,
-        fontWeight: 'bold', 
-        textShadowColor:'black', 
-        textShadowOffset:{width: 1, height: 1}, 
-        textShadowRadius: 2
-        }}>Select Muscle Focus:</Text>
-      <Picker
-      
-        selectedValue={muscleFocus}
-        onValueChange={(itemValue) => setMuscleFocus(itemValue)}
-        style={{paddingLeft: 15 ,borderRadius: 25, padding: 10, marginBottom: 20 ,backgroundColor: "#F2C49B"}}
-
-      >
-        <Picker.Item label="Choose One" value="Choose One"/>
-        <Picker.Item label="Chest" value="Chest" />
-        <Picker.Item label="Back" value="Back" />
-        <Picker.Item label="Legs" value="Legs" />
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Workout Directory</Text>
+        </View>
+  
+        <View style={styles.content}>
+          <View style={styles.logoContainer}>
+            <Text style={styles.logoText}>SmartFit</Text>
+            <Image
+              source={require('./assets/logo.png')}
+              style={styles.logoImage}
+            />
+          </View>
+  
+          <Text style={styles.label}>Select Muscle Focus:</Text>
+          <Picker
+            selectedValue={muscleFocus}
+            onValueChange={(itemValue) => setMuscleFocus(itemValue)}
+            style={styles.picker}
+          >
+            <Picker.Item label="Choose One" value="Choose One"/>
+            <Picker.Item label="Chest" value="Chest" />
+            <Picker.Item label="Back" value="Back" />
+            <Picker.Item label="Legs" value="Legs" />
         {/* Add more muscle focus options as needed */}
-      </Picker>
-
-      <Text style={{paddingBottom: 3, 
-        paddingLeft: 15, 
-        color: '#F2C49B', 
-        fontSize: 18,
-        fontWeight: 'bold',
-        textShadowColor:'black', 
-        textShadowOffset:{width: 1, height: 1}, 
-        textShadowRadius: 2
-        }}>Select Location:</Text>
-      <Picker
-        selectedValue={location}
-        onValueChange={(itemValue) => setLocation(itemValue)}
-        style={{paddingLeft: 15 ,borderRadius: 25, padding: 10, marginBottom: 20 ,backgroundColor: "#F2C49B"}}
-
-      >
-        <Picker.Item label="Choose One" value="Choose One"/>        
-        <Picker.Item label="Gym" value="Gym" />
-        <Picker.Item label="Home" value="Home" />
-      </Picker>
-
-      <Text style={{paddingBottom: 3, 
-        paddingLeft: 15, 
-        color: '#F2C49B', 
-        fontSize: 18,
-        fontWeight: 'bold', 
-        textShadowColor:'black', 
-        textShadowOffset:{width: 1, height: 1}, 
-        textShadowRadius: 2
-        }}>Select Equipment:</Text>
-      <Picker
-        selectedValue={equipment}
-        onValueChange={(itemValue) => setEquipment(itemValue)}
-        style={{paddingLeft: 15 ,borderRadius: 25, padding: 10, marginBottom: 20 ,backgroundColor: "#F2C49B"}}
-      >
-        <Picker.Item label="Choose One" value="Choose One"/>
-        <Picker.Item label="Dumbbells" value="Dumbbells" />
-        <Picker.Item label="Barbell" value="Barbell" />
-        <Picker.Item label="Resistance Bands" value="Resistance Bands" />
-        {/* Add more equipment options as needed */}
-      </Picker>
-      <Button
-        title="Submit"
-        onPress={handleSubmission}
-        style={{paddingVertical: 50, backgroundColor: '#4EA4D9' }}
-      />
-    </View>
-  );
-};
-
-export default WorkoutPage;
+          </Picker>
+  
+          <Text style={styles.label}>Select Location:</Text>
+          <Picker
+            selectedValue={location}
+            onValueChange={(itemValue) => setLocation(itemValue)}
+            style={styles.picker}
+          >
+            <Picker.Item label="Choose One" value="Choose One"/>        
+            <Picker.Item label="Gym" value="Gym" />
+            <Picker.Item label="Home" value="Home" />
+          </Picker>
+  
+          <Text style={styles.label}>Select Equipment:</Text>
+          <Picker
+            selectedValue={equipment}
+            onValueChange={(itemValue) => setEquipment(itemValue)}
+            style={styles.picker}
+          >
+            <Picker.Item label="Choose One" value="Choose One"/>
+            <Picker.Item label="Dumbbells" value="Dumbbells" />
+            <Picker.Item label="Barbell" value="Barbell" />
+            <Picker.Item label="Resistance Bands" value="Resistance Bands" />    
+          </Picker>
+  
+          <TouchableOpacity
+            style={styles.submitButton}
+            onPress={handleSubmission}
+          >
+            <Text style={styles.submitButtonText}>SUBMIT</Text>
+          </TouchableOpacity>
+          <Text style={styles.excerciseText}>Excercises</Text>
+        </View>
+      </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 20,
+      backgroundColor: "#F2856D",
+    },
+    header: {
+      marginBottom: 20,
+    },
+    headerText: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      color: '#fff',
+    },
+    content: {
+      flex: 1,
+    },
+    logoContainer: {
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    logoText: {
+      textAlign: "center",
+      fontSize: 24,
+      fontWeight: 'bold',
+      color: '#fff',
+      textShadowColor: '#F2C49B',
+      textShadowOffset: { width: 1, height: 1 },
+      textShadowRadius: 2,
+      //fontFamily: 'SpaceGrotesk',
+    },
+    logoImage: {
+      width: 200,
+      height: 200,
+      borderRadius: 15,
+    },
+    label: {
+      paddingBottom: 3,
+      paddingLeft: 15,
+      color: '#F2C49B',
+      fontSize: 18,
+      fontWeight: 'bold',
+      textShadowColor: 'black',
+      textShadowOffset: { width: 1, height: 1 },
+      textShadowRadius: 2,
+    },
+    picker: {
+      paddingLeft: 15,
+      borderRadius: 25,
+      padding: 10,
+      marginBottom: 20,
+      backgroundColor: "#F2C49B",
+    },
+    submitButton: {
+      width: "50%",
+      backgroundColor: "#72CEF2",
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: 25,
+      borderWidth: 2,
+      borderColor: "#3999d4",
+      alignSelf: 'center'
+    },
+    submitButtonText: {
+      textAlign: "center",
+      fontSize: 20,
+      paddingVertical: 5,
+      borderRadius: 25,
+      paddingBottom: 3,
+      paddingLeft: 15,
+      color: '#F2C49B',
+      fontWeight: 'bold',
+      textShadowColor: 'black',
+      textShadowOffset: { width: 1, height: 1 },
+      textShadowRadius: 2,
+    },
+    exercisesText:{
+      ontSize: 20,
+      paddingVertical: 5,
+      borderRadius: 25,
+      paddingBottom: 3,
+      paddingLeft: 15,
+      color: '#F2C49B',
+      fontWeight: 'bold',
+      textShadowColor: 'black',
+      textShadowOffset: { width: 1, height: 1 },
+      textShadowRadius: 2,
+    },
+  });
+  
+  export default WorkoutPage;
