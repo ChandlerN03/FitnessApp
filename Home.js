dac785a836498dc42562dfa0b4c155cceda106f0
ashwinthomas03 <118752427+ashwinthomas03@users.noreply.github.com>
2023-12-13 03:18:38 -0500

    Update Home.js

--- Home.js
+++ Home.js
@@ -3,15 +3,17 @@ import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import axios from "axios";
 
+
 function Home() {
   const navigation = useNavigation();
   const route = useRoute();
   const [memberInfo, setMemberInfo] = useState(null);
-  const { username } = route.params;
+  const {username} = route.params;
 
   useEffect(() => {
     axios.get(`http://localhost:3000/member/${username}`)
       .then(response => {
+        console.log("Member Info:", response.data.memberInfo);
         setMemberInfo(response.data.memberInfo);
       })
       .catch(error => {
@@ -23,6 +25,15 @@ function Home() {
     navigation.navigate("BMIpage");
   };
 
+  const goToWorkoutLog = () => {
+    navigation.navigate('WorkoutLog', { username: username })    
+};
+
+const goToWorkoutPage = () => {
+    navigation.navigate("WorkoutPage");
+  };
+
+
   return (
     <View style={styles.container}>
       {memberInfo && (
@@ -32,8 +43,8 @@ function Home() {
           <Text style={styles.cardText}>Age: {memberInfo.age}</Text>
           <Text style={styles.cardText}>Gender: {memberInfo.gender}</Text>
           <Text style={styles.cardText}>DOB: {memberInfo.DOB}</Text>
-          <Text style={styles.cardText}>Email: {memberInfo.email}</Text>
-          <Text style={styles.cardText}>Phone Number: {memberInfo.phone_number}</Text>
+          <Text style={styles.cardText}>Email: {memberInfo.email}</Text>   
+          <Text style={styles.cardText}>Phone Number: {memberInfo.Phone_number}</Text>
         </View>
       )}
       <TouchableOpacity
@@ -41,6 +52,17 @@ function Home() {
         onPress={goToBMIpage}
       >
         <Text style={styles.buttonText}>Go to BMI Calculator</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={goToWorkoutLog} style={styles.button1}>
+        <Text style={styles.buttonText}>Go to WorkoutLog</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button2}
+        onPress={goToWorkoutPage}
+      >
+        <Text style={styles.buttonText}>Go to WorkoutPage</Text>
       </TouchableOpacity>
     </View>
   );
@@ -59,17 +81,18 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   profileCard: {
-    backgroundColor: "rgba(255, 255, 255, 0.8)", // Semi-transparent white background
-    borderRadius: 20, // Adjust the border radius for a bigger card
-    padding: 30, // Adjust the padding for a bigger card
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    borderRadius: 50,
+    padding: 30,
     elevation: 5,
     position: "absolute",
     top: 20,
-    left: 20,
+    left: 10,
+    width: 300,
   },
   cardText: {
-    fontSize: 18, // Increase the font size for better readability
-    marginBottom: 15, // Adjust the margin for better spacing
+    fontSize: 18,
+    marginBottom: 15,
   },
   button: {
     backgroundColor: "#2596be",
@@ -77,12 +100,34 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     alignSelf: "center",
     position: "absolute",
-    bottom: 20,
+    bottom: 50,
   },
   buttonText: {
     color: "#fff",
     fontSize: 16,
   },
+  button1: {
+    backgroundColor: "#2596be",
+    padding: 15,
+    borderRadius: 4,
+    alignSelf: "center",
+    bottom: -470,
+  },
+  button2: {
+    backgroundColor: "#2596be",
+    padding: 15,
+    borderRadius: 5,
+    alignSelf: "center",
+    position: "absolute",
+    bottom: 20,
+    left: "50%",  // Adjust the left position based on your layout
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+
 });
+
 
 export default Home;
